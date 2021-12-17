022307840c1f7068c264d756ef391311c9ceec59
achal1912 <95094052+achal1912@users.noreply.github.com>
2021-12-17 14:56:48 +0530

    Add files via upload

---
+++ Anudip Assignment/Exercise 2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AchalConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(19+29);
+            Console.ReadLine();
+        }
+    }
+}
