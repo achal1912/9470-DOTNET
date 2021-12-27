73b5f312911a912ab0647a59f500bb165125a17a
achal1912 <95094052+achal1912@users.noreply.github.com>
2021-12-27 13:25:09 +0530

    Add files via upload

---
+++ Anudip Assignment/DEMO  2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System; 
+class  one { }
+class two  { }  
+
+namespace DEMO_2
+{
+    public class Program
+    {
+       public  static void test(object obj)
+        {
+            one x;
+            two y;
+            if (obj is one)
+            {
+                Console.WriteLine("class one"); 
+                x = (one)obj;
+            }
+            else if (obj is two)
+            {
+                Console.WriteLine("class two");
+                y = (two)obj;   
+            }
+            else
+            {
+                Console.WriteLine("None of the classes!");
+            }
+        }
+        public static void main()
+        {
+            one o1 = new one();
+            two o2 = new two();
+            test(o1);
+            test(o2);
+            test("str");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Anudip Assignment/NUMBER MANIPULATOR/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CALCULATORAPPLICATION
+{
+    class NumberManipulator
+    {
+        public void swrap(ref int x, ref int y)
+        {
+            int temp;
+            temp = x;
+            x = y;
+            y = temp;
+        }
+        static void main(string[] args)
+        {
+            NumberManipulator n = new NumberManipulator();
+            int a = 600;
+            int b = 100;
+            Console.WriteLine("before swap,value of a : {0}", a);
+            Console.WriteLine("before swap,value of b : {0}", b);
+            n.swrap(ref a, ref b);
+            Console.WriteLine("after swap,value of a : {0}", a);
+            Console.WriteLine("after swap,value of b : {0}", b);
+            Console.ReadLine();
+        }
+    }
+}
