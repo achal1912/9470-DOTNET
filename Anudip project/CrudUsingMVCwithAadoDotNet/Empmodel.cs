9517a45eb26d505998b6afeca7ece9e809c7bab0
achal1912 <95094052+achal1912@users.noreply.github.com>
2022-02-04 14:28:34 +0530

    Add files via upload

---
+++ Anudip project/CrudUsingMVCwithAadoDotNet/Empmodel.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUDUsingMVCwithAdoDotNet.Repository
+{
+    public class EmpModel
+    {
+        public object id { get; internal set; }
+        public object Name { get; internal set; }
+        public object City { get; internal set; }
+        public object Address { get; internal set; }
+        public int Empid { get; internal set; }
+    }
+}
