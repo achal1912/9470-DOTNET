c0f5b424364411d98bfb351f4c63cb6d08d6b805
achal1912 <95094052+achal1912@users.noreply.github.com>
2022-02-10 13:44:00 +0530

    Add files via upload

---
+++ Anudip practicals/LINKTOSQL2/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LINKTOSQL2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataClasses1DataContext db = new DataClasses1DataContext();
+            if (!Page.IsPostBack)
+            {
+                GridView1.DataSource = from ed in db.EmployeeDetails
+                                       select new
+                                       {
+                                           EmployeeName = ed.EmpName,
+                                           Location = ed.Location,
+                                           Gender = ed.Gender
+                                       };
+                GridView1.DataBind();
+            }
+
+
+        }
+    }
+}
