67bf2be39bb61627e1c123d15c4fb1637b074005
achal1912 <95094052+achal1912@users.noreply.github.com>
2022-02-14 16:53:55 +0530

    Add files via upload

---
+++ Anudip Assignment/LINQ_With_Object/LINQTOSQL01/StudentDetail.cs
@@ -0,0 +1,24 @@
+﻿namespace LINQTOSQL01
+{
+    internal class StudentDetail
+    {
+        private object sName;
+
+        public object GetSName()
+        {
+            return sName;
+        }
+
+        internal void SetSName(object value)
+        {
+            sName = value;
+        }
+
+        public StudentDetail(object sName) => this.SetSName(sName);
+
+        public object Sid { get; internal set; }
+        public object Location { get; internal set; }
+        public object Gender { get; internal set; }
+        public object Age { get; internal set; }
+    }
+}
