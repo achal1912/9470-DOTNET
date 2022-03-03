7a4f5b4e72e222c991495f4c00138e9a590e46a4
achal1912 <95094052+achal1912@users.noreply.github.com>
2022-03-03 12:49:23 +0530

    Add files via upload

---
+++ Anudip project/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MvcMultipleTable_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMultipleTable_Demo.ViewModel
+{
+    public class EmployeeViewModel
+    {
+        public IEnumerable<Employee> Employees { get; set; } //IEnumerable is an interface that defines one method getenumerator.It is a base interface of all non generic collection
+        public IEnumerable<Department> Departments { get; set; }
+        public IEnumerable<Incentive> Incentives { get; set; }
+    }
+}
